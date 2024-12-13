9efc57932e74f4fb48e73b8ef09bddce7fb3ee8c
Domminic <domminicm@unr.edu>
2024-12-12 17:00:38 -0800

    light mods

--- medpass/next.config.ts
+++ medpass/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  distDir: 'out',
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
