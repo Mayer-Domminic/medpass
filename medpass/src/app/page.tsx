b81cb0eaed3eb6ea9343e0ef1cf40afdf6807a11
Domminic <domminicm@unr.edu>
2025-02-05 19:37:02 -0800

    maybe fixing some bs

--- medpass/src/app/page.tsx
+++ medpass/src/app/page.tsx
@@ -8,31 +8,28 @@ export default function Home() {
   const { isAuthenticated, isLoading, checkAuth } = useAuth();
   const router = useRouter();
 
-  // Run checkAuth only on mount
   useEffect(() => {
     const authenticateUser = async () => {
-      await checkAuth();  // Check auth status after loading
+      await checkAuth();
     };
     
     authenticateUser();
   }, [checkAuth]);
 
-  // Render loading state while checking authentication
-  if (isLoading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
-      </div>
-    );
-  }
+  useEffect(() => {
+    if (!isLoading) {
+      if (isAuthenticated) {
+        router.push('/dashboard');
+      } else {
+        router.push('/auth/login');
+      }
+    }
+  }, [isLoading, isAuthenticated, router]);
 
-  // If authenticated, navigate to the Dashboard
-  if (isAuthenticated) {
-    router.push('/dashboard');
-    return null;  // Optionally show a loading screen here
-  }
-
-  // If not authenticated, you could render a message or a redirect to login page
-  router.push('/auth/login');
-  return null;
+  // Show loading state while checking authentication
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
+    </div>
+  );
 }
--- medpass/src/lib/api.ts
+++ medpass/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1';
+const API_URL = 'https://api.medpass.unr.dev';// TODO CHANGE THIS TO LOCAL WHEN DEVELOPING 'http://localhost:8000/api/v1';
 
 export const api = axios.create({
   baseURL: API_URL,
