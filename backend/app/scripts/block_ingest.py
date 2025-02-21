6c96f3b88271f9f4da3450eac5bd65cbf4d06f2d
David-Rino <rinod@unr.edu>
2025-02-20 23:16:20 -0800

    BLOCK DATA 2022-2023 INGESTION!!!!

--- backend/app/scripts/block_ingest.py
+++ backend/app/scripts/block_ingest.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import re
+from sqlalchemy.exc import IntegrityError
 from ..schemas import pydanticstudentinformation as pydantic
 from ..schemas import pydanticexamquestion as pydanticexam
 from ..schemas import pydanticclassfaculty as pydanticclass
@@ -10,56 +11,64 @@ from ..models import examquestionmodels as exammodel
 from ..core.database import get_db
 import os
 
-if __name__ == "__main__":
+def extract_subject(text):
+        result = re.search(r'Disciplines and Topics/([^%]+)', text)
+        return result.group(1).strip() if result else None
+
+#Extracts Year and Block Number 
+def process_sheet_name(df, sheet_number):
+    sheet_name = list(df.keys())[sheet_number]
     
-    filePath = os.path.join(
-        "app", "scripts", "data", "Deidentified Reports (1).xlsx"
-    )
+    match = re.search(r"^(\d{4})\s+Block\s+(\d+)", sheet_name)
     
+    if match:
+        year, block = match.groups()
+    
+    return sheet_name, year, block
+    
+    
+def ingest_class():
     class_filePath = os.path.join(
         "app", "scripts", "data", "Class Information.xlsx"
     )
     
     df_class = pd.read_excel(class_filePath, sheet_name=0)
     
-    df_unrdata = pd.read_excel(filePath, sheet_name=6)
-    
     #Import Base Class Information
+    db = next(get_db())
     try:
-        db = next(get_db())
         for _, row in df_class.iterrows():
-            class_data = pydanticclass.ClassModel(
-                ClassID = row['Class ID'],
-                ClassName = row['Class Name'],
-                ClassDescription = row['Description'],
-                Block = row['Block']
-            ) 
-            db_class_data = classmodel.Class(
-                classid = class_data.ClassID,
-                classname = class_data.ClassName,
-                classdescription = class_data.ClassDescription,
-                block = class_data.Block
-            )
-            db.add(db_class_data)
-        db.commit()
-        print('Base Class Data Loaded in Database')
-        
+            try:
+                class_data = pydanticclass.ClassModel(
+                    ClassID = row['Class ID'],
+                    ClassName = row['Class Name'],
+                    ClassDescription = row['Description'],
+                    Block = row['Block']
+                ) 
+                db_class_data = classmodel.Class(
+                    classid = class_data.ClassID,
+                    classname = class_data.ClassName,
+                    classdescription = class_data.ClassDescription,
+                    block = class_data.Block
+                )
+                db.add(db_class_data)
+                db.commit()
+            except IntegrityError:
+                print(f"Class {row['Class Name']} already exists in the database!")
+                db.rollback()
+            except Exception as e:
+                print(f"Error when adding data {e}")
+                db.rollback()
+                raise
+        print('Base Class Data Loaded in Database')    
     except Exception as e:
-        print(f"Error when adding data {e}")
-        db.rollback()
-        raise
-    
+        print(f"Unexpected Error {e}")
     finally:
-        db.close()  
-    
-    number_subjects = len(df_unrdata.columns[1:]) // 3
-    groupavg_indx = df_unrdata[df_unrdata.iloc[:, 0] == 'Group Average'].index[0]
-    
-    print(f'Number of subjects for block{number_subjects}')
-    
-    def extract_subject(text):
-        result = re.search(r'Disciplines and Topics/([^%]+)', text)
-        return result.group(1).strip() if result else None
+        db.close()
+        
+def process_sheet_data(data):
+    number_subjects = len(data.columns[1:]) // 3
+    groupavg_indx = data[data.iloc[:, 0] == 'Group Average'].index[0]
     
     topics_data = []
     student_scores = []
@@ -67,21 +76,21 @@ if __name__ == "__main__":
     for sub_indx in range(number_subjects):
         '''Splits Block Into three sections for subjects'''
         start = 1 + (sub_indx * 3)
-        subject_column = df_unrdata.columns[start:start + 3] 
+        subject_column = data.columns[start:start + 3] 
         
         topic_name = extract_subject(subject_column[0])
         '''Convert to string first to strip % convert back to float after'''
-        group_avg = float(str(df_unrdata.iloc[groupavg_indx, start]).rstrip('%')) * 100
+        group_avg = float(str(data.iloc[groupavg_indx, start]).rstrip('%')) * 100
         
-        data = {
+        topic_header = {
             'name': topic_name,
-            'assessment total': int(df_unrdata.iloc[0, start]),
-            'total items': int(df_unrdata.iloc[1, start]),
+            'assessment total': int(data.iloc[0, start]),
+            'total items': int(data.iloc[1, start]),
             'group_average': group_avg
         }
-        topics_data.append(data)
+        topics_data.append(topic_header)
         
-        student_rows = df_unrdata[df_unrdata.index > groupavg_indx].copy()
+        student_rows = data[data.index > groupavg_indx].copy()
         
         for _, row in student_rows.iterrows():
             try:
@@ -96,18 +105,16 @@ if __name__ == "__main__":
             except (ValueError, TypeError) as e:
                 print(f"Can not process score for studentID: {row.iloc[0]} in subject {topic_name}: {e}")
                 continue
-    #print(topics_data)
-    #print(student_scores)
-    #Ingesting GradeOffering with a Block Number temp hard coded
-    
+            
+    return topics_data, student_scores
+
+def insert_class_offering(db, block_num, year):
     try:
-        db = next(get_db())
-        block_num = 1
         class_id = db.query(classmodel.Class.classid).filter(classmodel.Class.block == block_num).first()
-            #Temp Will Regex Date 
+
         offering_data = pydanticclass.ClassOffering(
             ClassID = int(class_id[0]),
-            DateTaught = 2024
+            DateTaught = year
         ) 
         db_offering_data = classmodel.ClassOffering(
             classid = offering_data.ClassID,
@@ -115,22 +122,23 @@ if __name__ == "__main__":
         )
         db.add(db_offering_data)
         db.commit()
-        print('Base Class Data Loaded in Database')
-        
+        print(f'Class Offering Data for ClassID: {class_id} for Year: {year} Loaded in Database')
+    except IntegrityError:
+        print(f"Class Offering with ClassID: {class_id} already exists in the database!")
+        db.rollback ()
     except Exception as e:
         print(f"Error when adding data {e}")
         db.rollback()
-        raise
-    finally:
-        db.close()  
+        raise 
+       
+    return db_offering_data.classofferingid
         
-    #Temp Ingestion of Grade Classification
-    try:
-        db = next(get_db())
-        #Class offering is temp hard coded
-        for subject in topics_data:
+def insert_grade_classifications(db, topics_data, class_offering_id):
+    classification_ids = {}
+    for subject in topics_data:
+        try:
             classification_data = pydanticclass.GradeClassification(
-                ClassOfferingID = 1,
+                ClassOfferingID = class_offering_id,
                 ClassificationName = subject['name'],
                 UnitType = 'Assessment'
             ) 
@@ -140,47 +148,88 @@ if __name__ == "__main__":
                 unittype = classification_data.UnitType
             )
             db.add(db_classification_data)
-        db.commit()
-        print('Grade Classification Data Loaded in Database')
-        
-    except Exception as e:
-        print(f"Error when adding data {e}")
-        db.rollback()
-        raise
-    finally:
-        db.close()  
-        
-    #Ingesting All Student Grades
-    try:
-        db = next(get_db())
-        for subject in topics_data:
-            classificationid = db.query(classmodel.GradeClassification.gradeclassificationid).filter(classmodel.GradeClassification.classificationname == subject['name']).first()
-            for row in student_scores:
-                #temp just hard coded grade classification id
-                student_data = pydanticclass.StudentGrade(
-                    StudentID = row['student id'],
-                    GradeClassificationID = int(classificationid[0]),
-                    PointsEarned = row['points achieved'],
-                    PointsAvailable = row['points available']
-                ) 
-                db_student_data = classmodel.StudentGrade(
-                    studentid = student_data.StudentID,
-                    gradeclassificationid = student_data.GradeClassificationID, 
-                    pointsearned = student_data.PointsEarned,
-                    pointsavailable = student_data.PointsAvailable
-                )
-                db.add(db_student_data)
-        db.commit()
-        print('Student Block Data Loaded in Database')
-        
-    except Exception as e:
-        print(f"Error when adding data {e}")
-        db.rollback()
-        raise
-    
-    finally:
-        db.close()  
-
+            db.commit()
+            classification_ids[subject['name']] = db_classification_data.gradeclassificationid
+                
+        except IntegrityError:
+            print(f"Grade Classfication with name: {subject['name']} already exists in the database!")
+            db.rollback() 
             
-    #print(topics_data)
-    #print(student_scores)
+        except Exception as e:
+            print(f"Error when adding data {e}")
+            db.rollback()
+            raise
+    print(f'Grade Classification Data for: {class_offering_id} Loaded in Database')
+        
+    return classification_ids
+
+def insert_student_grades(db, student_scores, classification_ids, block):
+    for row in student_scores:
+        try:
+            classification_id = classification_ids.get(row['subject name'])
+                    #temp just hard coded grade classification id
+            student_data = pydanticclass.StudentGrade(
+                StudentID = row['student id'],
+                GradeClassificationID = classification_id,
+                PointsEarned = row['points achieved'],
+                PointsAvailable = row['points available']
+            ) 
+            db_student_data = classmodel.StudentGrade(
+                studentid = student_data.StudentID,
+                gradeclassificationid = student_data.GradeClassificationID, 
+                pointsearned = student_data.PointsEarned,
+                pointsavailable = student_data.PointsAvailable
+            )
+            db.add(db_student_data)
+            db.commit()      
+        except IntegrityError:
+            print(f"Student Grade with already exists for student: {row['student id']} already exists in the database!")
+            db.rollback() 
+        
+        except Exception as e:
+            print(f"Error when adding data {e}")
+            db.rollback()
+            raise
+    print(f'Student Grade Data for Block: {block} Loaded in Database')
+    
+
+if __name__ == "__main__":
+    
+    filePath = os.path.join(
+        "app", "scripts", "data", "Deidentified Reports (1).xlsx"
+    )
+    
+    df_unrdata = pd.read_excel(filePath, sheet_name=None)
+    dropped_sheets = [
+        'Full ID List', '2024 Block 1 Block Filters', '2024 Block 2 Block Filters', '2024 Block 3', '2024 Block 4', 
+        '2024 Block 5', '2024 Block 6 Block Filters', '2024 Block 6 Category Filters', '2024 Block 7 Block Filters', '2024 Block 7 Category Filters', '2024 Block 8 Block Filters', '2024 Block 8 Category Filters', '2024 Block 9', '2024 Block 10'
+    ]
+    df_unrdata = {name: df for name, df in df_unrdata.items() if name not in dropped_sheets}
+    sheet_names = df_unrdata.keys()
+    #Ingesting Base Class Information
+    ingest_class()
+    
+    
+    #Ingestion Process Begins
+    for i in range(len(sheet_names)):
+        sheet_name, year, block = process_sheet_name(df_unrdata, i)
+        
+        df_current = df_unrdata[sheet_name]
+        try:
+            db = next(get_db())
+            topics_data, student_scores = process_sheet_data(df_current)
+            
+            class_offering_id = insert_class_offering(db, int(block), int(year))
+            
+            classification_ids = insert_grade_classifications(db, topics_data, class_offering_id)
+            
+            insert_student_grades(db, student_scores, classification_ids, block)
+            
+            print(f"Succesfully process sheet {sheet_name}")
+            
+        except Exception as e:
+            print(f"Error processing sheet {sheet_name}: {e}")
+            db.rollback()
+        finally:
+            db.close()   
+        
