81e334e63cfbcb6d494e20e19aeb1dbeb6f5379d
nvisitacion <nolan.visitacion0920@gmail.com>
2025-04-18 18:00:49 -0700

    faculty path included

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
-from .endpoints import auth, risk, settings, student, report, settings, about
+from .endpoints import auth, risk, settings, student, report, settings, faculty, about 
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(risk.router, prefix="/info", tags=["info"])
 api_router.include_router(student.router, prefix="/student", tags=["student"])
+api_router.include_router(faculty.router, prefix="/faculty", tags=["faculty"])
 api_router.include_router(report.router, prefix="", tags=["report"])
 api_router.include_router(settings.router, prefix="/settings", tags=["settings"])
 api_router.include_router(about.router, prefix="/about", tags=["about"])
