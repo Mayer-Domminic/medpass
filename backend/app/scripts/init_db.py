db8f1026c1068fa80890855d0bd2ce8a79000eca
David-Rino <rinod@unr.edu>
2025-04-19 20:11:31 -0700

    Added Sample Question Ingestion for Testing

--- backend/app/scripts/init_db.py
+++ backend/app/scripts/init_db.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     os.system('python -m app.scripts.tables.create_db')
     os.system('python -m app.scripts.id_ingest')
     os.system('python -m app.scripts.block_ingest')
+    os.system('python -m app.scripts.question_ingest')
     print("Database has been reset & reseeded!")
