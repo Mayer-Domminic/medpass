57bf48fc5fd795a08f5e6c34e52aab51938fb9f8
Jake Herweg <jherweg@unr.edu>
2025-04-22 20:22:04 -0700

    Added calendar page redirect and commented out mock_risk endpoint for sake of passing the github  tests

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
-from .endpoints import auth, mock_risk, settings, student, report, calendar, faculty, about 
+# use mock_risk when using calendar page for now
+# from .endpoints import auth, mock_risk, settings, student, report, calendar, faculty, about 
+from .endpoints import auth, risk, settings, student, report, calendar, faculty, about 
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(mock_risk.router, prefix="/info", tags=["info"])
+# api_router.include_router(mock_risk.router, prefix="/info", tags=["info"])
+api_router.include_router(risk.router, prefix="/info", tags=["info"])
 api_router.include_router(student.router, prefix="/student", tags=["student"])
 api_router.include_router(faculty.router, prefix="/faculty", tags=["faculty"])
 api_router.include_router(report.router, prefix="", tags=["report"])
